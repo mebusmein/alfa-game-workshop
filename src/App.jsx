da2990850c2542659358d755567334cf727e4612
Tim Mein <mebusmein@gmail.com>
2023-04-20 17:12:14 +0200

    add canvas and 3d player

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Box } from '@react-three/drei'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function Stage() {
+
+  const boxRef = useRef()
+
+  useFrame(({ }) => {
+    boxRef.current.rotation.x += 0.01
+    boxRef.current.rotation.y += 0.01
+  })
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+    <Box ref={boxRef} args={[1, 1, 1]} position={[0, 0, 0]}>
+      <meshStandardMaterial color="hotpink" />
+    </Box>
+    </>
+  )
+}
+
+
+
+function App() {
+  return (
+    <Canvas camera={{
+      position: [0, 2, 10],
+      fov: 75,
+    }}>
+      <ambientLight  />
+      <pointLight position={[10, 10, 10]} />
+      <Stage />
+    </Canvas>
   )
 }
 
